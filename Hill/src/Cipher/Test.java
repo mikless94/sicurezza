23917220522fd31569dcfcd2b0b6e7001436dcf1
dems93 <Demetrio Laveglia@Demetrio>
2017-10-09 20:44:20 +0200

    Finished Test Excercise 1

--- Hill/src/Cipher/Test.java
+++ Hill/src/Cipher/Test.java
@@ -1,15 +1,50 @@
 package Cipher;
 
+import javax.swing.JOptionPane;
+import javax.swing.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		Hill h = new Hill();
 		String ciphertext = "";
+		JFrame frame = new JFrame("Test");
 		
-		try {
-			h.setKey("aede");
+		Object[] options = {"Yes", "No"};
+		int n = JOptionPane.showOptionDialog(frame,"Would you generate your key randomly?","Key Generator",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+		
+		
+		if (n == 0){
+			System.out.println("Chiave generata automaticamente!");
+			h.genKey();
+			System.out.println("Chiave utilizzata per cifrare: " + h.getKey());
+			String plaintext = JOptionPane.showInputDialog("Messaggio da cifrare:");
+			System.out.println ("La stringa da cifrare è: " + plaintext);
+			ciphertext = h.enc(plaintext);
+			System.out.println ("La stringa cifrata è: " + ciphertext);
+			String plaintextDec = h.dec(ciphertext);
+			System.out.println ("La stringa decifrata è: " + plaintextDec);			
+		}
+		
+		else{
+			try{
+				String key = JOptionPane.showInputDialog("Inserire la chiave di 4 caratteri compresi nel dizionario:");
+				h.setKey(key);
+				System.out.println("Chiave utilizzata per cifrare: " + h.getKey());
+				String plaintext = JOptionPane.showInputDialog("Messaggio da cifrare:");
+				System.out.println ("La stringa da cifrare è: " + plaintext);
+				ciphertext = h.enc(plaintext);
+				System.out.println ("La stringa cifrata è: " + ciphertext);
+				String plaintextDec = h.dec(ciphertext);
+				System.out.println ("La stringa decifrata è: " + plaintextDec);		
+			} catch(InvalidKeyException ex) {};
+		}
+		
+		/*try {
+			h.setKey("lidh");
 			System.out.println(h.getKey());
-			String plaintext = "demetrio giuseppe michele";
+			String plaintext = "ciao";
 			System.out.println ("La stringa da cifrare è: " + plaintext);
 
 			ciphertext = h.enc(plaintext);
@@ -17,7 +52,7 @@ public class Test {
 			
 			String plaintextDec = h.dec(ciphertext);
 			System.out.println ("La stringa decifrata è: " + plaintextDec);
-		}catch(InvalidKeyException ex) {};
+		} catch(InvalidKeyException ex) {};*/
 	}
 }
 
