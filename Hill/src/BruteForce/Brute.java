bfd3e7fcae4d5eee806afd4c5530bb037790bbca
mikless94 <mikless94@gmail.com>
2017-10-09 22:54:44 +0200

    idee brute force

--- Hill/src/BruteForce/Brute.java
+++ Hill/src/BruteForce/Brute.java
@@ -8,7 +8,9 @@ public class Brute {
 		//determinante non deve avere fattori comuni con la base modulare. 
 		//tale rischio è scongiurato se il modulo è un numero primo (come accade in questo caso)
 		
-		
+		//idea possibile --> modificare dec in modo tale da considerare mano mano i digrammi ottenuti
+		//se il digramma ottenuto non è possibile nella lingua (italiano o inglese)
+		//saltare alla prossima chiave
 
 	}
 
