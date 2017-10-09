2c320d2711dfff11e71a6f386575c65fbeeb541e
mikless94 <mikless94@gmail.com>
2017-10-09 22:21:42 +0200

    aggiunto package BruteForce

---
+++ Hill/src/BruteForce/Brute.java
@@ -0,0 +1,15 @@
+package BruteForce;
+
+public class Brute {
+
+	public static void main(String[] args) {
+		//proprietà di una generica chiave di Hill:7
+		//determinante != 0
+		//determinante non deve avere fattori comuni con la base modulare. 
+		//tale rischio è scongiurato se il modulo è un numero primo (come accade in questo caso)
+		
+		
+
+	}
+
+}
