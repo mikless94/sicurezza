7a9856e7ba8f734bbde86b7ea0e6f5b49204e68b
dems93 <Demetrio Laveglia@Demetrio>
2017-10-09 21:29:50 +0200

    Finished First Excerise

---
+++ Hill/src/Cipher/InvalidPlaintext.java
@@ -0,0 +1,10 @@
+package Cipher;
+
+public class InvalidPlaintext extends Exception{
+	
+	public InvalidPlaintext(String string) {
+		// TODO Auto-generated constructor stub
+		System.out.println(string);
+	}
+
+}
