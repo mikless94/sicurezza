054a82ff5f2e4db821561142b526f5399ebd5451
Giu67890 <giuseppe.santoriello2@gmail.com>
2017-10-12 19:21:31 +0200

    1) Impostazione delle classi dell'esercizio 4.
    2) Implementazione delle classi definite, con metodi per l'acquisizione
    da file, e non, per il testo cifrato e i digrammi.

---
+++ Hill/src/FrequencyAnalysis/Digram.java
@@ -0,0 +1,71 @@
+package FrequencyAnalysis;
+
+public class Digram implements Comparable<Digram> {
+	
+	private String firstCharacter;
+	private String secondCharacter;
+	private int frequency;
+	
+	
+	public Digram(String firstCharacter, String secondaCharacter, int frequency) {
+		this.firstCharacter = firstCharacter;
+		this.secondCharacter = secondaCharacter;
+		this.frequency = frequency;
+	}
+	public String getFirstCharacter() {
+		return firstCharacter;
+	}
+	public void setFirstCharacter(String firstCharacter) {
+		this.firstCharacter = firstCharacter;
+	}
+	public String getSecondCharacter() {
+		return secondCharacter;
+	}
+	public void setSecondCharacter(String secondaCharacter) {
+		this.secondCharacter = secondaCharacter;
+	}
+	public int getFrequency() {
+		return frequency;
+	}
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	
+	public String printDigram() {
+		String s = "";
+		s += this.getFirstCharacter() + '\t' + this.getSecondCharacter() + '\t' 
+				+ Integer.toString(this.getFrequency());
+		return s;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + frequency;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Digram other = (Digram) obj;
+		if (frequency != other.frequency)
+			return false;
+		return true;
+	}
+	@Override
+	public int compareTo(Digram o) {
+		int freq = this.frequency - o.frequency;
+	if(freq > 0) 
+		return 1;
+	else if (freq < 0) 
+		return -1;
+	else
+		return 0;
+	}
+	
+}
---
+++ Hill/src/FrequencyAnalysis/Test.java
@@ -0,0 +1,13 @@
+package FrequencyAnalysis;
+
+public class Test {
+
+	public static void main(String[] args) {
+		DigramsCollection coll = new DigramsCollection();
+		coll.takeDigramsFromFile("Jones2004_Bigram.txt");
+		System.out.println("Ci sono: " + coll.sizeOfCollection() + " digrammi.\n" + coll.printCollection());
+		CryptoAnalysis data = new CryptoAnalysis();
+		data.readCipherTextFromFile("ciphertext.txt");
+		System.out.println("Testo cifrato: \n" + data.getCipherText());
+		}
+	}
