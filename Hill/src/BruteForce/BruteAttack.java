ff1f7958f5121a20ed8496944d830e6c5ebd3fb7
mikless94 <mikless94@gmail.com>
2017-10-17 23:16:57 +0200

    aggiunto test forza bruta
    manca selezione utente del numero di consonanti consecutive

--- Hill/src/BruteForce/BruteAttack.java
+++ Hill/src/BruteForce/BruteAttack.java
@@ -9,53 +9,65 @@ import Cipher.InvalidKeyException;
 public class BruteAttack {
 	
 	//proprietà di una generica chiave di Hill:7
-			//determinante != 0
-			//determinante non deve avere fattori comuni con la base modulare. 
-			//tale rischio è scongiurato se il modulo è un numero primo (come accade in questo caso)
-			
-			//idea possibile --> una volta decodificato applicare le euristiche
-			//se euristica non è rispettata saltare alla prossima chaive
-			
-			//va bene sia considerare euristiche separate per italiano e inglese o euristiche congiunte
-			private int num_keys = 0;
-			private Hill h = new Hill();
-			private String k = "";
-			private int word_length = 9;
-			
-			//Pattern patternIta = Pattern.compile("((([b-df-hj-np-tv-z])(?!\\2)){3})");
-			//Pattern patternIta = Pattern.compile("th|wh|,[a-z']|[a-z&&[^aeiou]]{3,5}""(.+)\\1+");
-			public void attack (String cipherText) {
-				Pattern patternIta = Pattern.compile("(.)\\1{2,}|,\\S|'{2,}|\\s{2,}|[a-z&&[^aeiou]]{4,}|\\w{"+word_length+",}\\b");
-				
-				for (int i=0; i<29; i++) {
-					for (int j=0; j<29; j++) {
-						for (int l=0; l<29; l++) {
-							for (int n=0; n<29; n++) {
-								
-								k = "";
-								k += Hill.getReversedDict().get(i);
-								k += Hill.getReversedDict().get(j);
-								k += Hill.getReversedDict().get(l);
-								k += Hill.getReversedDict().get(n); 
-								
-								try {
-									h.setKey(k);
-								} catch(InvalidKeyException ex) {
-									continue;
-								};
-				
-								String plainText = h.dec(cipherText);
-								Matcher matcherIta = patternIta.matcher(plainText);
-								if (!matcherIta.find()) {
-									num_keys++;
-									System.out.println(plainText);
-								}
-							}	
+	//determinante != 0
+	//determinante non deve avere fattori comuni con la base modulare. 
+	//tale rischio è scongiurato se il modulo è un numero primo (come accade in questo caso)
+	
+	//idea possibile --> una volta decodificato applicare le euristiche
+	//se euristica non è rispettata saltare alla prossima chaive
+	
+	private int num_keys = 0;
+	private Hill h = new Hill();
+	private String k = "";
+	private int wordLength;
+	
+	/**
+	 * @return the wordLength
+	 */
+	public int getwordLength() {
+		return wordLength;
+	}
+
+	/**
+	 * @param wordLength the wordLength to set
+	 */
+	public void setwordLength(int wordLength) {
+		this.wordLength = wordLength;
+	}
+
+	public void attack (String cipherText) {
+		Pattern patternIta = Pattern.compile("(.)\\1{2,}|,\\S|'{2,}|\\s{2,}|[a-z&&[^aeiou]]{5,}|\\w{"+wordLength+",}\\b");
+		
+		for (int i=0; i<29; i++) {
+			for (int j=0; j<29; j++) {
+				for (int l=0; l<29; l++) {
+					for (int n=0; n<29; n++) {
+						
+						k = "";
+						k += Hill.getReversedDict().get(i);
+						k += Hill.getReversedDict().get(j);
+						k += Hill.getReversedDict().get(l);
+						k += Hill.getReversedDict().get(n); 
+						
+						try {
+							h.setKey(k);
+						} catch(InvalidKeyException ex) {
+							continue;
+						};
+		
+						String plainText = h.dec(cipherText);
+						Matcher matcherIta = patternIta.matcher(plainText);
+						if (!matcherIta.find()) {
+							num_keys++;
+							System.out.println(plainText);
 						}
-					}
+					}	
 				}
-				System.out.println("numero chiavi:"+num_keys);
 			}
+		}
+		System.out.println("numero chiavi con lunghezza parola "+wordLength+":"+num_keys+"\n");
+	
+	}
 }
 
 
--- Hill/src/BruteForce/BruteTest.java
+++ Hill/src/BruteForce/BruteTest.java
@@ -1,8 +1,32 @@
 package BruteForce;
 
-public class BruteTest {
+import javax.swing.JOptionPane;
 
+import Cipher.InvalidKeyException;
+
+
+public class BruteTest {
+	final static int DEFAULT_WORD_LENGTH = 3;
+	private static int wordLength = DEFAULT_WORD_LENGTH;
+	
 	public static void main(String[] args) {
+		BruteAttack brute = new BruteAttack();
+		Object[] options = {"Yes", "No"};
+		int n = JOptionPane.showOptionDialog(null,"Would you choose maximum word length?\nWarning: Default word length is "+DEFAULT_WORD_LENGTH,"Brute Force Attack",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+		
+		
+		if (n == 0)
+			wordLength =Integer.parseInt( JOptionPane.showInputDialog("Insert maximum word length:"));
+		
+		brute.setwordLength(wordLength);
+		do {
+			brute.attack("gbgbemumlcdvbb,izn qxpmwatoehldvmg qqumnivlw jmwpoeiyxyhnemwu w,u mnjidqo,fddqdvcvswumlcdvcvswumoe");
+			n = JOptionPane.showOptionDialog(null,"Have you found a result?","Brute Force Attack",
+					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+			if (n != 0)
+				brute.setwordLength(brute.getwordLength()+1);
+		} while (n!=0);	
 
 	}
 
