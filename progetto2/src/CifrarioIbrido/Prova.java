2944ed02fb229bfdf520a87f85682109ab5689f1
mikless94 <mikless94@gmail.com>
2017-11-05 15:04:48 +0100

    aggiustata firma e file

--- progetto2/src/CifrarioIbrido/Prova.java
+++ progetto2/src/CifrarioIbrido/Prova.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
@@ -19,21 +20,24 @@ import javax.crypto.NoSuchPaddingException;
 public class Prova {
 	private static Incapsula inc = new Incapsula( );
 	
-	public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, SignatureException {
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, SignatureException, InvalidAlgorithmParameterException {
 		// TODO Auto-generated method stub
 		
-		Files.deleteIfExists(Paths.get("./publicKeyFile.txt"));
+		/*Files.deleteIfExists(Paths.get("./publicKeyFile.txt"));
 		Files.deleteIfExists(Paths.get("./fileToSend.txt"));
 		Files.deleteIfExists(Paths.get("./digKeysFile.txt"));
 		Files.deleteIfExists(Paths.get("./myTempFile.txt"));
 		
 		inc.addUser("Michele", 1024, "PKCS1Padding"); 
 		inc.addUser("Giuseppe", 2048, "PKCS1Padding");
-		inc.addUser("Giovanni", 2048, "PKCS1Padding");
-		inc.deleteUser("Giovanni");
+		inc.addUser("Giovanni", 2048, "PKCS1Padding");*/
+		//inc.deleteUser("Giovanni");
 		
-		inc.messageToSend ("Michele", "Michele", "DES", "CBC", "PKCS5Padding", "C:\\Users\\Michele\\Desktop\\jamesharden.jpg", 1024, "SHA1withDSA");
-		inc.decodeMessage("C:\\Users\\Michele\\git\\sicurezza\\progetto2\\fileToSend.txt", "C:\\Users\\Michele\\Desktop\\jameshardendecodificato.jpg" );
+		//inc.messageToSend ("Michele", "Giuseppe", "AES", "CFB", "PKCS5Padding", "C:\\Users\\Michele\\Desktop\\jamesharden.jpg"/*, 1024, "SHA1withDSA"*/);
+		//inc.decodeMessage("C:\\Users\\Michele\\git\\sicurezza\\progetto2\\fileToSend.txt", "C:\\Users\\Michele\\Desktop\\jameshardendec.jpg" );
+		
+		//inc.messageToSend ("Michele", "Giovanni", "DESede", "CBC", "PKCS5Padding", "C:\\Users\\Michele\\Desktop\\messaggio.txt", 2048, "SHA224withDSA");
+		//inc.decodeMessage("C:\\Users\\Michele\\git\\sicurezza\\progetto2\\fileToSend.txt", "C:\\Users\\Michele\\Desktop\\messaggiodec.txt" );
 	}
 
 }
