3c6b0673da6829a64a0d7ca38af9ae44ffbffa85
mikless94 <mikless94@gmail.com>
2017-12-10 19:47:24 +0100

    completate generazione share e ricostruzione segreto

--- progetto4/src/progetto4/shamir/Prova.java
+++ progetto4/src/progetto4/shamir/Prova.java
@@ -6,14 +6,27 @@ import java.util.ArrayList;
 public class Prova {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//test generazione shares
 		BigInteger [] a = new BigInteger [] {BigInteger.valueOf(11),BigInteger.valueOf(2)};
 		BigInteger [] shares;
-		shares = SecretSharing.generateShares(new BigInteger ("12"), a);
+		BigInteger prime = BigInteger.valueOf(19);
+		shares = SecretSharing.generateShares(new BigInteger ("12"), a, prime);
 		System.out.println("shares");
 		for (BigInteger k:shares) {
-			System.out.println(k);
+			System.out.print(k+"\t");
 		}
+		
+		//test ricostruzione segreto
+		prime = BigInteger.valueOf(11);
+		BigInteger [] participants1 = new BigInteger [] {BigInteger.valueOf(1),BigInteger.valueOf(4)};
+		BigInteger [] participants2 = new BigInteger [] {BigInteger.valueOf(2),BigInteger.valueOf(0)};
+		BigInteger [] participants3 = new BigInteger [] {BigInteger.valueOf(5),BigInteger.valueOf(4)};
+		ArrayList <BigInteger []> info = new ArrayList <BigInteger []> ();
+		info.add(participants1);
+		info.add(participants2);
+		info.add(participants3);
+		BigInteger secret = SecretSharing.rebuildSecret(info, prime);
+		System.out.println("\nsegreto ricostruito "+secret);		
 	}
 
 }
--- progetto4/src/progetto4/shamir/SecretSharing.java
+++ progetto4/src/progetto4/shamir/SecretSharing.java
@@ -5,30 +5,35 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class SecretSharing {
+	private static final int n=5;
+	private static final int k=3;
 	
-	
-	
-	//s appartenente a Zp
-	public static BigInteger [] generateShares (BigInteger secret, BigInteger [] coeff ) {
-		int n = 5;
+	public static BigInteger [] generateShares (BigInteger secret, BigInteger [] coeff, BigInteger prime ) {
+		
 		BigInteger [] shares = new BigInteger [n];
 		for (int x = 1; x<=n; x++) {
 			shares[x-1] = secret;
 			for (int i=0; i<=coeff.length-1; i++) 
 				shares [x-1] = shares[x-1].add(coeff[i].multiply(BigInteger.valueOf((long)Math.pow(x,i+1))));
-			shares[x-1] = shares[x-1].remainder(BigInteger.valueOf(19));
+			shares[x-1] = shares[x-1].remainder(prime);
 		}
 		return shares;
 	}
 	 
 	
 	//necessario avere identità partecipante / share 
-	public BigInteger rebuildSecret (ArrayList <BigInteger []> info ) {
+	public static BigInteger rebuildSecret (ArrayList <BigInteger []> info, BigInteger prime) {
 		BigInteger secret = BigInteger.ZERO;
+		BigInteger currentValue = BigInteger.ZERO;
 		for (int i=0; i<info.size(); i++) {
-			int current = i;
-			secret = secret.add(info.get(i)[1]);
+			currentValue = info.get(i)[1];
+			for (int j=0; j<info.size(); j++) {
+				if (j!=i) 
+					currentValue = currentValue.multiply(info.get(j)[0].negate().multiply((info.get(i)[0].subtract(info.get(j)[0])).modInverse(prime)).mod(prime)).mod(prime);
+			}
+			secret = secret.add(currentValue);
 		}
+		secret = secret.remainder(prime);
 		return secret;
 	}
 }
